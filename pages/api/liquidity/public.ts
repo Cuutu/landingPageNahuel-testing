2012ba35dfe3c8cc8b6e092024d0a2db094eb167
Tortu <tortuwu028@gmail.com>
2025-09-09 23:06:14 -0300

    cambianod lo de liquidez

--- pages/api/liquidity/public.ts
+++ pages/api/liquidity/public.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/googleAuth';
 import dbConnect from '@/lib/mongodb';
-import Liquidity from '@/models/Liquidity';
+import Liquidity from '../../../models/Liquidity';
 import User from '@/models/User';
 
 interface PublicLiquidityResponse {
