21fbb5312a21633e6adbe14a14e66ac5b5aa2750
Cuutu <32869813+Cuutu@users.noreply.github.com>
2025-11-30 20:15:18 -0300

    OPÉARCIONES

--- pages/api/operations/delete.ts
+++ pages/api/operations/delete.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]';
+import { authOptions } from '@/lib/googleAuth';
 import dbConnect from '@/lib/mongodb';
 import Operation from '@/models/Operation';
 
